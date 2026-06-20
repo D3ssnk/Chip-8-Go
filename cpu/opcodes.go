be0f1d9fe006a83097786b544503237a3a28f5c2
D3ssnk <65511755+D3ssnk@users.noreply.github.com>
2026-06-20 11:13:46 +0100

    implemented return opcode and the tests

--- cpu/opcodes.go
+++ cpu/opcodes.go
@@ -1,5 +1,17 @@
 package cpu
 
+import "errors"
+
 func (cpu *CPU) clear() {
 	cpu.display = [32][8]uint8{}
+}
+
+func (cpu *CPU) ret() error{
+	if cpu.sp == 0 {
+		return errors.New("Stack is empty")
+	}
+
+	cpu.pc = cpu.stack[cpu.sp]
+	cpu.sp--
+	return nil
 }
